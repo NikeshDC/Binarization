7c75030bf336309f7a3b4456b503d45565da256d
NikeshDC <60224139+NikeshDC@users.noreply.github.com>
2022-10-13 16:46:38 +0545

    added image writing method

--- Java/EasyRead/ImageUtility.java
+++ Java/EasyRead/ImageUtility.java
@@ -7,20 +7,26 @@ public class ImageUtility
 {
     public static Image readImage(String filepath)
     {
+        File file = new File(filepath);
+        return readImage(file);
+    }
+    public static Image readImage(File file)
+    {
         //read from image file and return it
         BufferedImage bimage = null;
         try{
-            bimage = ImageIO.read(new File(filepath));
+            bimage = ImageIO.read(file);
         }
         catch(IOException e)
         {
             System.out.println("Image File error");
             System.exit(0);
         }
-        Image img = new Image(bimage.getWidth(), bimage.getHeight());
         
-        for(int i=0; i<img.getWidth(); i++)
-            for(int j=0; j<img.getHeight(); j++)
+        Image image = new Image(bimage.getWidth(), bimage.getHeight());
+        
+        for(int i=0; i<image.getWidth(); i++)
+            for(int j=0; j<image.getHeight(); j++)
             {
                 int pix;
                 short r,g,b, gray;
@@ -29,8 +35,45 @@ public class ImageUtility
                 g = (short)((pix>>8) & 0x000000ff);
                 b = (short)((pix) & 0x000000ff);
                 gray = (short)(0.21 * r + 0.72 * g + 0.07 * b);
-                img.pixel[i][j] = gray;
+                image.pixel[i][j] = gray;
             }
-        return img;
+        return image;
+    }
+    
+    public static void writeImage(Image image, String filename, String format, boolean binarized)
+    {
+        BufferedImage bimage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+        
+        //create buffered image
+        if (binarized)
+        {
+            for(int i=0; i<image.getWidth(); i++)
+                for(int j=0; j<image.getHeight(); j++)
+                {
+                    if(image.pixel[i][j] == 0)
+                        bimage.setRGB(i,j, 0xffffffff);
+                    else
+                        bimage.setRGB(i,j, 0x000000);
+                    //bimage.setRGB(i,j, image.pixel[i][j]);
+                }
+        }
+        else
+        {
+            for(int i=0; i<image.getWidth(); i++)
+                for(int j=0; j<image.getHeight(); j++)
+                {
+                    bimage.setRGB(i,j, image.pixel[i][j]);
+                }
+            
+        }
+        
+        File out = new File(filename);
+        try{
+            ImageIO.write(bimage, format, out);
+        }
+        catch(IOException e)
+        {
+            System.out.println("Couldnot save image");
+        }
     }
 }
