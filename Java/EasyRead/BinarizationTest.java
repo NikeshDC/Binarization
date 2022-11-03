192a85bb7d98a85ed7fb15b3cf3751be7339e06b
NikeshDC <60224139+NikeshDC@users.noreply.github.com>
2022-11-03 13:21:17 +0545

    Update BinarizationTest.java

--- Java/EasyRead/BinarizationTest.java
+++ Java/EasyRead/BinarizationTest.java
@@ -2,9 +2,10 @@ import javax.swing.*;
 
 public class BinarizationTest
 {
-    public static void main(String[] args)
+    public static void test()
     {
-        String imagepath = "a.png";
+        String imagepath = "a-n.png";
+        String savepath = "save-n.png";
         //String imagepath = args[0];
         JFrame window = new JFrame();
         ImagePanel myimgpanel = new ImagePanel();
@@ -13,18 +14,40 @@ public class BinarizationTest
         OCR ocr = new OCR();
         OCR.Binarization bin = ocr.new Binarization();
         //float k = Float.parseFloat(args[0]);
-        float k = 0.3f;
+        float k = 0.20f;
         int w = 25;
         //int w = Integer.parseInt(args[1]);
-        OCR.Binarization.Otsu otsu = bin.new Otsu();
+        //OCR.Binarization.Otsu otsu = bin.new Otsu();
         OCR.Binarization.Sauvola sauv = bin.new Sauvola(k, w);
         ocr.setImage(img);
-        otsu.binarize();
-        //sauv.binarize();
-        sauv.binarize(otsu.threshold);
         
+        //##image preprocessing
+        // ImageWindow imageWindow = new ImageWindow(img, w);
+        // int imageMean = imageWindow.getImageMean();
+        // int imageVar = imageWindow.getImageVariance(imageMean);
+        // int imageSD = (int)Math.sqrt(imageVar);
+        // Image enhancedImage = ImageUtility.enhanceText(img, imageMean, imageSD);
+        // ocr.setImage(enhancedImage);
+        
+        //otsu.binarize();
+        sauv.binarize();
+        //sauv.binarize(otsu.threshold, 0.05f);
+        
+        //CLOSE operation: dilation and then erosion
+        //int dilErodWinSize = 3;
+        //Image dilatedBinImage = ImageUtility.dilate(sauv.binarizedImage, dilErodWinSize);
+        //Image erodedBinImage = ImageUtility.erode(dilatedBinImage, dilErodWinSize);
+        
+        //OPEN operation: dilation and then erosion
+        //int dilErodWinSize = 3;
+        //Image dilatedBinImage = ImageUtility.dilate(sauv.binarizedImage, dilErodWinSize);
+        //Image erodedBinImage = ImageUtility.erode(sauv.binarizedImage, dilErodWinSize);
+        
+        //ImageUtility.writeImage(sauv.binarizedImage, savepath, "jpg", true); //saving output
        
+        //##setting image to display
         myimgpanel.setImg(sauv.binarizedImage, true);
+        //myimgpanel.setImg(enhancedImage);
         
         window.getContentPane().add(myimgpanel);
         
